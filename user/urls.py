b43c0de3a15ae5075e0a085da4e232b2b84bab8d
wlllssd <511872591@qq.com>
2018-12-16 22:41:00 +0800

    RIGHT MODIFY

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     re_path('song/(?P<id>[0-9]+)/', views.user_songs, name='song'),
-    # re_path('user2/(?P<id>[0-9]+)/', views.album, name='user2'),
     re_path('playlist/(?P<id>[0-9]+)/', views.user_playlist, name='playlist'),
 ]
